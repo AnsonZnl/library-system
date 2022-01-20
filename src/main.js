f871b95740623e218f0ff61b4fb6d6d90ed2faa3
zhangningle <zhangningle@58.com>
2022-01-20 14:58:18 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,22 @@
+import Vue from "vue";
+import App from "./App.vue";
+import Cloudbase from "@cloudbase/vue-provider";
+
+// 注意更新此处的TCB_ENV_ID为你自己的环境ID
+window._tcbEnv = window._tcbEnv || {
+    TCB_ENV_ID: "library-system-dev-3dm1b8e388467",
+};
+
+export const envId = window._tcbEnv.TCB_ENV_ID;
+export const region = window._tcbEnv.TCB_REGION;
+
+Vue.config.productionTip = false;
+
+Vue.use(Cloudbase, {
+    env: envId,
+    region: region
+});
+
+new Vue({
+    render: h => h(App)
+}).$mount("#app");
